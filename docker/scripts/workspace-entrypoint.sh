9bf21f49c36b60fa25c82128c49863586e3c44a0
icolson <icolson@nilfisk.com>
2023-08-16 12:49:10 -0500

    workspace entrypoint

--- docker/scripts/workspace-entrypoint.sh
+++ docker/scripts/workspace-entrypoint.sh
@@ -1,4 +1,63 @@
+
 #!/bin/bash
+
+#
+
+# Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
+
+#
+
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+
+# and proprietary rights in and to this software, related documentation
+
+# and any modifications thereto. Any use, reproduction, disclosure or
+
+# distribution of this software and related documentation without an express
+
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+
+ 
+
+# Build ROS dependency
+
+echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
+
+source /opt/ros/${ROS_DISTRO}/setup.bash
+
+ 
+
+#sudo apt-get update
+
+#rosdep update
+
+ 
+
+# Restart udev daemon
+
+sudo service udev restart
+
+ 
+
+echo "source /workspaces/isaac_ros-dev/install/setup.bash" >> ~/.bashrc
+
+source /workspaces/isaac_ros-dev/install/setup.bash
+
+#ros2 launch yolox_ros_cpp yolox_tensorrt_jetson.launch.py &
+
+ros2 launch micro_ros_agent micro_ros_agent_launch.py &
+
+pip install typing-extensions --upgrade
+
+python3 /workspaces/isaac_ros-dev/src/backend_components/backend_ui_server/backend_ui_server/main.py --conn_string_path /usr/config/connection.txt --default_config_path /workspaces/isaac_ros-dev/src/backend_components/backend_ui_server/backend_ui_server/default_machine_config.json --config_path /workspaces/isaac_ros-dev/src/backend_components/backend_ui_server/backend_ui_server/machine_config.json
+
+ 
+
+ 
+
+$@
+
+# add ! bin bash back here
 #
 # Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
 #
@@ -9,27 +68,27 @@
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
 
 # Build ROS dependency
-echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
-source /opt/ros/${ROS_DISTRO}/setup.bash
+# echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
+# source /opt/ros/${ROS_DISTRO}/setup.bash
 
-# sudo apt-get update
-# rosdep update
+# # sudo apt-get update
+# # rosdep update
 
-# Restart udev daemon
-sudo service udev restart
+# # Restart udev daemon
+# sudo service udev restart
 
-echo "source /workspaces/isaac_ros-dev/install/setup.bash" >> ~/.bashrc
-source /workspaces/isaac_ros-dev/install/setup.bash
+# echo "source /workspaces/isaac_ros-dev/install/setup.bash" >> ~/.bashrc
+# source /workspaces/isaac_ros-dev/install/setup.bash
 
-# Setup before starting BE server
-sudo chown 1000:1000 /usr/config/
-pip3 install typing-extensions --upgrade
+# # Setup before starting BE server
+# sudo chown 1000:1000 /usr/config/
+# pip3 install typing-extensions --upgrade
 
-# Start the applications
-#ros2 launch yolox_ros_cpp yolox_tensorrt_jetson.launch.py &
-python3 /workspaces/isaac_ros-dev/src/backend_components/backend_ui_server/backend_ui_server/main.py \
-    --conn_string_path /usr/config/connection.txt \
-    --default_config_path /workspaces/isaac_ros-dev/src/backend_components/backend_ui_server/backend_ui_server/default_machine_config.json \
-    --config_path /usr/config/machine_config.json &
-ros2 launch micro_ros_agent micro_ros_agent_launch.py
-$@
+# # Start the applications
+# #ros2 launch yolox_ros_cpp yolox_tensorrt_jetson.launch.py &
+# python3 /workspaces/isaac_ros-dev/src/backend_components/backend_ui_server/backend_ui_server/main.py \
+#     --conn_string_path /usr/config/connection.txt \
+#     --default_config_path /workspaces/isaac_ros-dev/src/backend_components/backend_ui_server/backend_ui_server/default_machine_config.json \
+#     --config_path /usr/config/machine_config.json &
+# ros2 launch micro_ros_agent micro_ros_agent_launch.py
+# $@
