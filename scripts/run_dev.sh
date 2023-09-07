9e43a441587cc99eba18473016310f71e746564f
Matt Stiehm <mstiehm@nilfisk.com>
2023-09-07 14:30:40 -0500

    adding changes to build oak-d layer

--- scripts/run_dev.sh
+++ scripts/run_dev.sh
@@ -182,7 +182,7 @@ docker run -it --rm \
     --network host \
     ${DOCKER_ARGS[@]} \
     -v $ISAAC_ROS_DEV_DIR:/workspaces/isaac_ros-dev \
-    -v /dev/*:/dev/* \
+    -v /dev/:/dev/ \
     -v /etc/localtime:/etc/localtime:ro \
     -v /usr/config:/usr/config \
     --name "$CONTAINER_NAME" \
