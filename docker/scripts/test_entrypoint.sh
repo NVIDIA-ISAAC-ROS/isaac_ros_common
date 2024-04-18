bf9592b8642cf8f2b28387521e7ffbeb559f0115
Matt Stiehm <mstiehm@nilfisk.com>
2024-04-18 16:45:45 -0500

    removed sudo

--- docker/scripts/test_entrypoint.sh
+++ docker/scripts/test_entrypoint.sh
@@ -105,7 +105,7 @@ source /workspaces/isaac_ros-dev/install/setup.bash
 
 rm -rf /workspaces/isaac_ros-dev/test_results
 
-sudo mkdir -p /workspaces/isaac_ros-dev/test_results
+mkdir -p /workspaces/isaac_ros-dev/test_results
 
 echo Starting Tests...
 
