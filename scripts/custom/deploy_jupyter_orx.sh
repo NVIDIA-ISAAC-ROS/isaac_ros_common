a41ba271b47f98bf2aa61c24d1fa087edb5c454b
vschorp <vincent.schorp@balgrist.ch>
2024-11-07 15:31:31 +0100

    ros2 and jupyter working

--- scripts/custom/deploy_jupyter_orx.sh
+++ scripts/custom/deploy_jupyter_orx.sh
@@ -20,11 +20,11 @@ sudo docker run --rm -it --gpus all --runtime=nvidia \
     --network host \
     -e RMW_IMPLEMENTATION=rmw_cyclonedds_cpp \
     -e ROS_DOMAIN_ID=1 \
-    -e CYCLONEDDS_URI=/home/admin/cyclone_profile_dgx.xml \
+    -e CYCLONEDDS_URI=/home/jovyan/cyclone_profile_dgx.xml \
     -e ROS_ROOT=/opt/ros/humble \
-    -v /home/adm_girf/cyclone_profile_dgx.xml:/home/admin/cyclone_profile_dgx.xml \
-    -v /home/adm_girf/data:/home/admin/data \
-    --user admin \
-    --workdir /home/admin \
-    vschorp98/orx-middleware-isaac-ros-"$PLATFORM_NAME"-ros2_humble \
+    -v /home/adm_girf/cyclone_profile_dgx.xml:/home/jovyan/cyclone_profile_dgx.xml \
+    -v /home/adm_girf/data:/home/jovyan/data \
+    --user jovyan \
+    --workdir /home/jovyan \
+    vschorp98/orx-middleware-isaac-ros-"$PLATFORM_NAME"-jupyter \
     bash
