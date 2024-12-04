e5f6b68666e691e2c1ba9592f609edfc8f8b0d3d
vschorp <vincent.schorp@balgrist.ch>
2024-12-04 09:13:26 +0000

    add hdmi-input

---
+++ scripts/custom/deploy_hdmi_input.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Determine platform architecture
+PLATFORM="$(uname -m)"
+if [[ $PLATFORM == "aarch64" ]]; then
+    PLATFORM_NAME="jetson"
+elif [[ $PLATFORM == "x86_64" ]]; then
+    PLATFORM_NAME="desktop"
+else
+    echo "Unsupported platform: $PLATFORM"
+    exit 1
+fi
+
+# # Set default absolute path for the config file
+# default_config_path="/home/vschorp/dev/orx/data/experiment_config/datahub_01/zed_mini_0"
+
+# # Use the first argument as the config path, or the specified default path
+# config_path="${1:-$default_config_path}"
+
+# # Check if the config file exists
+# if [ ! -f "$config_path" ]; then
+#     echo "Configuration file not found at: $config_path"
+#     exit 1
+# fi
+
+docker run --rm -it --gpus all --runtime=nvidia \
+    --privileged \
+    --network host \
+    -e ROS_DOMAIN_ID=1 \
+    -e RMW_IMPLEMENTATION=rmw_cyclonedds_cpp \
+    -e CYCLONEDDS_URI=/home/admin/cyclone_profile.xml \
+    -v /home/"$USER"/dev/orx/cyclone_profile.xml:/home/admin/cyclone_profile.xml \
+    -v /dev:/dev \
+    -e DATAHUB_NAME=datahub_01 \
+    vschorp98/orx-middleware-isaac-ros-"$PLATFORM_NAME"-hdmi_input
