89cdf364dc9ecd4beb42cb1e14e993d50c6c1b24
frederic giraud <frederic.giraud@balgrist.ch>
2025-01-27 08:31:32 +0100

    update configuration for fri kuka deployment and add custom scripts

---
+++ scripts/custom/deploy_fri_kuka.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Determine platform architecture
+PLATFORM="$(uname -m)"
+if [[ $PLATFORM == "aarch64" ]]; then
+    PLATFORM_NAME="jetson"
+elif [[ $PLATFORM == "x86_64" ]]; then
+    PLATFORM_NAME="desktop"
+else
+    echo "Unsupported platform: $PLATFORM"
+    exit 1
+fi
+
+# Set default absolute path for the config file
+default_config_path="/home/fred/Documents/config_middleware"
+
+# Use the first argument as the config path, or the specified default path
+config_path="${1:-$default_config_path}"
+
+# Check if the config dir exists
+if [ ! -d "$config_path" ]; then
+    echo "Configuration dir not found at: $config_path"
+    exit 1
+fi
+
+docker_name=$(basename ${config_path})
+
+docker run --rm -it --gpus all --runtime=nvidia \
+    --name $docker_name \
+    --privileged \
+    --network host \
+    -e ROS_DOMAIN_ID=1 \
+    -v "$config_path":/home/admin/fri_kuka_config \
+    girf/orx-middleware-isaac-ros-"$PLATFORM_NAME"-fri-kuka
---
+++ scripts/custom/run_dev_sony_a7r.sh
@@ -0,0 +1,1 @@
+./../run_dev.sh -i ros2_humble -a "-v /home/fred/Documents/config_middleware:/home/admin/config" -a "-v /dev/input:/dev/input" -b
