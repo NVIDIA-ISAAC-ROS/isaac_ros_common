68b45d1465fb3f7097027fe66f1dedb45031cac2
vschorp <vincent.schorp@balgrist.ch>
2025-04-09 16:48:26 +0000

    add deploy us server and add deploy zed dgx

---
+++ scripts/custom/deploy_us_server.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Determine platform architecture
+PLATFORM="$(uname -m)"
+if [[ $PLATFORM == "aarch64" ]]; then
+    PLATFORM_NAME="jetson"
+elif [[ $PLATFORM == "x86_64" ]]; then
+    PLATFORM_NAME="desktop"
+else
+    echo "Unsupported platform: $PLATFORM"
+    exit 1
+fi
+
+# Set default absolute path for the config file
+default_config_path="/home/$USER/dev/orx/us_server_config/config.yaml"
+
+# Use the first argument as the config path, or the specified default path
+config_path="${1:-$default_config_path}"
+
+# Check if the config dir exists
+if [ ! -d "$config_path" ]; then
+    echo "Configuration dir not found at: $config_path"
+    exit 1
+fi
+
+docker_name=$(basename ${config_path})
+
+docker run --rm -it --gpus all --runtime=nvidia \
+    --name $docker_name \
+    --privileged \
+    --network host \
+    -e DISPLAY=$DISPLAY \
+    -e ROS_DOMAIN_ID=1 \
+    -e RMW_IMPLEMENTATION=rmw_cyclonedds_cpp \
+    -e CYCLONEDDS_URI=/home/admin/cyclone_profile.xml \
+    -v /home/"$USER"/dev/orx/cyclone_profile.xml:/home/admin/cyclone_profile.xml \
+    vschorp98/orx-middleware-isaac-ros-"$PLATFORM_NAME"-us-server
---
+++ scripts/custom/deploy_zed_dgx.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Determine platform architecture
+PLATFORM="$(uname -m)"
+if [[ $PLATFORM == "aarch64" ]]; then
+    PLATFORM_NAME="jetson"
+elif [[ $PLATFORM == "x86_64" ]]; then
+    PLATFORM_NAME="desktop"
+else
+    echo "Unsupported platform: $PLATFORM"
+    exit 1
+fi
+
+# Set default absolute path for the config file
+default_config_path="/home/$USER/dev/orx/experiment_config/dgx/zed_streaming/datahub_01_zed_mini_0"
+
+# Use the first argument as the config path, or the specified default path
+config_path="${1:-$default_config_path}"
+
+# Check if the config file exists
+if [ ! -f "$config_path" ]; then
+    echo "Configuration file not found at: $config_path"
+    exit 1
+fi
+
+docker_name=$(basename ${config_path})
+
+docker run --rm -it --gpus all --runtime=nvidia \
+    --name $docker_name \
+    --privileged \
+    --network host \
+    -e ROS_DOMAIN_ID=1 \
+    -e RMW_IMPLEMENTATION=rmw_cyclonedds_cpp \
+    -e CYCLONEDDS_URI=/home/admin/cyclone_profile.xml \
+    -v /home/"$USER"/dev/orx/cyclone_profile.xml:/home/admin/cyclone_profile.xml \
+    -v /dev/input:/dev/input \
+    -v /tmp/:/tmp/ \
+    -v "/home/"$USER"/dev/orx/zed/settings:/usr/local/zed/settings" \
+    -v "/home/"$USER"/dev/orx/zed/resources:/usr/local/zed/resources" \
+    -v "$config_path":/zed_mini_ros_config.yaml \
+    vschorp98/orx-middleware-isaac-ros-"$PLATFORM_NAME"-zed
