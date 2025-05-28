b987f494cca4885a07d71a722614b6a5a429b568
Nino Louman <nino.louman@gmail.com>
2025-05-28 16:48:16 +0200

    Slight improvements & Jetson docker built

--- scripts/custom/deploy_depthai.sh
+++ scripts/custom/deploy_depthai.sh
@@ -12,15 +12,30 @@ else
 fi
 
 # Set default absolute path for the config file
-default_config_path="/home/$USER/git/orx_middleware/orx_interface/device_config_gui/example_config/datahub_03/depthai_cam"
+default_config_path="/home/$USER/dev/orx/data/experiment_config/datahub_03/depthai_oakdprow_0"
 
 # Use the first argument as the config path, or the specified default path
 config_path="${1:-$default_config_path}"
-Check if the config file exists
+
+# Check if the config file exists
 if [ ! -f "$config_path" ]; then
     echo "Configuration file not found at: $config_path"
     exit 1
 fi
+
+# # Run this the first time when deploying the DepthAI camera
+# default_udev_rules_path="/home/$USER/dev/orx/orx_middleware/isaac_ros_common/docker/udev_rules/80-movidius.rules"
+# # Check if the udev rules file exists
+# if [ ! -f "$default_udev_rules_path" ]; then
+#     echo "Udev rules file not found at: $default_udev_rules_path"
+#     exit 1
+# fi
+# # Copy udev rules to the system
+# sudo cp "$default_udev_rules_path" /etc/udev/rules.d/80-movidius.rules
+# # Reload udev rules
+# sudo udevadm control --reload-rules
+# # Trigger udev to apply the new rules
+# sudo udevadm trigger
 
 docker_name=$(basename ${config_path})
 
@@ -34,5 +49,5 @@ docker run --rm -it\
     -v /home/$USER/git/orx_middleware/cyclone_profile.xml:/home/admin/cyclone_profile.xml \
     -v "$config_path":/home/admin/depthai_cam.yaml \
     -v /dev/:/dev/ \
-    peetzha/orx-middleware-isaac-ros-"$PLATFORM_NAME"-depthai \
+    girf/orx-middleware-isaac-ros-"$PLATFORM_NAME"-depthai \
 
